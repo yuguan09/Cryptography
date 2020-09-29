116e364218c0d15a8c03ccb6f1b0b2570081e1c5
yuguan09 <38544697+yuguan09@users.noreply.github.com>
2020-09-29 20:08:22 +0900

    Add files via upload

---
+++ Cryptography1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<math.h>
+#define N 10000
+#define shift 2
+
+int main(void)
+{
+	int i;
+	char input[N];
+	char output[N];
+
+	for (i = 0; i < N; i++)
+	{
+		input[i] = getchar();
+		output[i] = input[i] + shift;
+		//printf("%c,%c\n",input[i],output[i]);
+		//printf("%d,%d\n",input[i],output[i]);
+		if (input[i] == '^')break;
+	}
+	//printf("%d\n",i);
+
+	for (int k = 0;k < i;k++)
+	{
+		printf("%c", output[k]);
+	}
+}
+
+	
