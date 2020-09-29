d2884ed4a5bcee99c22f7bafa8b178c5fd65eea4
yuguan09 <38544697+yuguan09@users.noreply.github.com>
2020-09-29 21:46:32 +0900

    Add files via upload

--- Cryptography1.c
+++ Cryptography1.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include<math.h>
-#define N 10000
-#define shift 2
+
+#define N 10000　　　　//入力したい文字の数が事前確定できないので、なるべく大きく設定します。
+#define shift 2         //シフト位数を確定する
 
 int main(void)
 {
 	int i;
-	char input[N];
-	char output[N];
+	char input[N];　　　//明文の部分
+	char output[N];　　　//暗号化された部分
 
-	for (i = 0; i < N; i++)
+	for (i = 0; i < N; i++)　　
 	{
-		input[i] = getchar();
-		output[i] = input[i] + shift;
-		//printf("%c,%c\n",input[i],output[i]);
-		//printf("%d,%d\n",input[i],output[i]);
-		if (input[i] == '^')break;
-	}
+		input[i] = getchar();　　　　　//getcharという関数を使って、入力した文字をchar型の配列として保存します。
+		output[i] = input[i] + shift;　//ASCIIコードから、入力した文字をシフトし、新しい文字になります。
+		//c言語のデータ構造で,char型の変量は実際int型の変量として保存されているので、
+		if (input[i] == '^')break;　　　　　//“＾”という符号が入力終了という意味で設定します。　
+	}　　　　　
 	//printf("%d\n",i);
 
 	for (int k = 0;k < i;k++)
