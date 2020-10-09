da577180df58f639289b1e9f669a7272d6cc7cc4
yuguan09 <38544697+yuguan09@users.noreply.github.com>
2020-10-09 13:38:55 +0900

    Add files via upload

--- vigen.c
+++ vigen.c
@@ -1,19 +1,13 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<cs50.h>
-#include<math.h>
 
 int main(void)
 {
     char mat[26][26];
 
-    //mat[0][0]=65;
-    //printf("%c\n",mat[0][0]);
+    for(int j=0;j<=25;j++){
 
-    for(int j=0;j<=25;j++)
-    {
-        for(int k=0;k<=25;k++)
-        {
+        for(int k=0;k<=25;k++){
+
             int i=j+k;
             mat[j][k]=i+65;
             if(i>25)for(int l=0;l<i-25;l++)
